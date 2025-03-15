5cb0d172a25af415da6af631007d271de7ee0fb2
Agrim Gupta <agrimgupt195@gmail.com>
2025-03-15 06:00:15 +0530

    fixed error in setting cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,13 +21,13 @@ const setCookie = (res,accessToken,refreshToken) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
         sameSite: "Strict",
-        secure: true,
+        secure: process.env.NODE_ENV === "production",
         maxAge: 15 * 60 * 1000
     });
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         sameSite: "Strict",
-        secure: true,
+        secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 }
